b70704f8df24b3844f03638ce8189d6a4996c90a
Joshua Cornutt <jcornutt@gmail.com>
2016-11-09 19:07:47 -0500

    Styles update

--- web/app/themes/Sparks-Consulting/rowingdb/components/main/main.php
+++ web/app/themes/Sparks-Consulting/rowingdb/components/main/main.php
@@ -1,26 +1,30 @@
 <ng-include src="getSidebar()"></ng-include>
-<div class="map large-8 columns">
-	<header>
-		<h5>My Priorities:</h5>
-		<select ng-model="filter.priority">
-			<option value="Enrollment">Enrollment</option>
-			<option value="Religion">Religious Affiliation</option>
-		</select>
-		<select>
-			<option>Secondary</option>
-		</select>
-		<select>
-			<option>Tertiary</option>
-		</select>
-	</header>
-	<div class="map">
-	<h1>{{filtered.length}} Colleges</h1>
-	<a ui-sref="listing">Show Schools</a>
-	<ul ng-repeat="college in colleges | filter:filter.religion | filter:query as filtered">
-		<li>
-			{{college.acf.school_zip}}
-		</li>
-	</ul>
-	</div>
 
+<div class="rowing-priorities-wrapper">
+  <div class="map large-8 columns">
+    <header class="rowing-priorities-header">
+      <h5>My Priorities:</h5>
+      <div class="rowing-priorities">
+        <select class="rowing-priority large-4 columns" ng-model="filter.priority">
+          <option value="Enrollment">Enrollment</option>
+          <option value="Religion">Religious Affiliation</option>
+        </select>
+        <select class="rowing-priority large-4 columns">
+          <option>Secondary</option>
+        </select>
+        <select class="rowing-priority large-4 columns">
+          <option>Tertiary</option>
+        </select>
+      </div>
+    </header>
+    <div class="map">
+    <h1>{{filtered.length}} Colleges</h1>
+    <a ui-sref="listing">Show Schools</a>
+    <ul ng-repeat="college in colleges | filter:filter.religion | filter:query as filtered">
+      <li>
+        {{college.acf.school_zip}}
+      </li>
+    </ul>
+    </div>
+  </div>
 </div>
--- web/app/themes/Sparks-Consulting/rowingdb/components/sidebar/sidebar.php
+++ web/app/themes/Sparks-Consulting/rowingdb/components/sidebar/sidebar.php
@@ -1,25 +1,27 @@
-<div class="filter-bar large-4 columns" ng-controller="SidebarController">
-	<h3>Filters:</h3>
-	<a ng-click='reset()'>Reset</a>
-	<ul>
-		<li><a ng-controller="ProgramFilter" ng-click='open()'>Program Type</a></li>
-		<li><a ng-controller="EnrollmentFilter" ng-click='open()'>Enrollment</a></li>
-		<li><a>Tuition</a></li>
-		<li><a>Financial Aid Score</a></li>
-		<li><a>Classroom Ratio</a></li>
-		<li><a>Environment</a></li>
-		<li><a>Location</a></li>
-		<li><a>Selectivity</a></li>
-		<li><a>Academic Intensity</a></li>
-		<li><a>Private/Public</a></li>
-		<li><a ng-controller="ReligionFilter" ng-click='open()'>Religious Affiliation</a></li>
-		<li><a>Majors</a></li>
-		<li><a>Food Service</a></li>
-		<li><a>Housing, Types</a></li>
-		<li><a>Housing, Sub-types</a></li>
-		<li><a>Housing, Alcohol</a></li>
-	</ul>
-	<ng-include src="getReligion()"></ng-include>
-	<ng-include src="getProgram()"></ng-include>
-	<ng-include src="getEnrollment()"></ng-include>
+<div class="filter-bar-wrapper">
+  <div class="filter-bar large-4 columns" ng-controller="SidebarController">
+    <h3>Filters:</h3>
+    <a ng-click='reset()'>Reset</a>
+    <ul>
+      <li><a ng-controller="ProgramFilter" ng-click='open()'>Program Type</a></li>
+      <li><a ng-controller="EnrollmentFilter" ng-click='open()'>Enrollment</a></li>
+      <li><a>Tuition</a></li>
+      <li><a>Financial Aid Score</a></li>
+      <li><a>Classroom Ratio</a></li>
+      <li><a>Environment</a></li>
+      <li><a>Location</a></li>
+      <li><a>Selectivity</a></li>
+      <li><a>Academic Intensity</a></li>
+      <li><a>Private/Public</a></li>
+      <li><a ng-controller="ReligionFilter" ng-click='open()'>Religious Affiliation</a></li>
+      <li><a>Majors</a></li>
+      <li><a>Food Service</a></li>
+      <li><a>Housing, Types</a></li>
+      <li><a>Housing, Sub-types</a></li>
+      <li><a>Housing, Alcohol</a></li>
+    </ul>
+    <ng-include src="getReligion()"></ng-include>
+    <ng-include src="getProgram()"></ng-include>
+    <ng-include src="getEnrollment()"></ng-include>
+  </div>
 </div>
