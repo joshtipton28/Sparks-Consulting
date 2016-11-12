3be944174c21569d1a9081874d33232c4b5708bb
Joshua Cornutt <jcornutt@gmail.com>
2016-11-12 15:02:07 -0500

    Generalizing iface

--- web/app/themes/Sparks-Consulting/rowingdb/components/sidebar/sidebarController.js
+++ web/app/themes/Sparks-Consulting/rowingdb/components/sidebar/sidebarController.js
@@ -14,6 +14,20 @@ app.controller('SidebarController', ['$scope', 'Filter', function($scope, Filter
 
 	$scope.filter = Filter;
 
+  $scope.filterIsActive = function(filter) {
+    var ret = false;
+    if( angular.isArray(filter) )
+      angular.forEach(filter, function(val) {
+        if( val ) {
+          ret = true;
+          return;
+        }
+      });
+    else if( filter )
+      ret = true;
+    return ret;
+  };
+
   $scope.reset = function() {
     $scope.filter.filters = {};
   };
