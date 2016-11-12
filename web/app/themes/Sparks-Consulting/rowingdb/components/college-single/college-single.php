abac95e7e206c75761741c6cf0ebf116f9b6ae97
Joshua Cornutt <jcornutt@gmail.com>
2016-11-11 20:22:42 -0500

    Generalizing iface

--- web/app/themes/Sparks-Consulting/rowingdb/components/college-single/college-single.php
+++ web/app/themes/Sparks-Consulting/rowingdb/components/college-single/college-single.php
@@ -12,35 +12,35 @@
         </button>
       </div>
     </header>
-  </div>
 
-  <div class="large-6 columns">
-    <div class="college-single-stats-list">
-      <table>
-        <tbody>
-          <tr>
-            <td>Women's Program(s):</td>
+    <div class="large-6 columns">
+      <div class="college-single-stats-list">
+        <table>
+          <tbody>
             <tr>
-              Division II, Openweight<br />
-              Women's Rowing Program Website
+              <td>Women's Program(s):</td>
+              <tr>
+                Division II, Openweight<br />
+                Women's Rowing Program Website
+              </tr>
             </tr>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
-
-  <div class="large-6 columns">
-    <div class="college-single-image">
-      Image goes here
+          </tbody>
+        </table>
+      </div>
     </div>
 
-    <div class="college-single-related-info">
-      Related Info goes here
-    </div>
+    <div class="large-6 columns">
+      <div class="college-single-image">
+        Image goes here
+      </div>
 
-    <div class="college-single-majors">
-      Majors goes here
+      <div class="college-single-related-info">
+        Related Info goes here
+      </div>
+
+      <div class="college-single-majors">
+        Majors goes here
+      </div>
     </div>
   </div>
 </div>
