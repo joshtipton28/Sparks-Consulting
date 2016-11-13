023bc47addc986abc496cc8e97da510c19f55b88
Joshua Cornutt <jcornutt@gmail.com>
2016-11-13 10:57:25 -0500

    Generalizing iface

--- web/app/themes/Sparks-Consulting/rowingdb/components/main/main.php
+++ web/app/themes/Sparks-Consulting/rowingdb/components/main/main.php
@@ -37,7 +37,7 @@
               keyboard-shortcuts="false"
               style="height:100%;">
         <marker ng-repeat="college in colleges | filter:masterFilter | filter:hasLocationFilter as filtered"
-                position="{{college.acf.school_city}}, {{college.acf.school_state}}"></marker>
+                position="{{trustHtml(college.title.rendered)}}, {{college.acf.school_city}}, {{college.acf.school_state}}"></marker>
       </ng-map>
     </div>
   </div>
