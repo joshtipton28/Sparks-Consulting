a2ce605fbb78b9f9d71daae35f04ff61901a3bed
Joshua Cornutt <jcornutt@gmail.com>
2016-11-13 10:20:04 -0500

    Generalizing iface

--- web/app/themes/Sparks-Consulting/library/enqueue-scripts.php
+++ web/app/themes/Sparks-Consulting/library/enqueue-scripts.php
@@ -29,11 +29,6 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	// It's a good idea to do this, performance-wise. No need to load everything if you're just going to use the grid anyway, you know :)
 	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/javascript/foundation.js', array('jquery'), '2.3.0', true );
 
-// Angular "count-to" directive
-  wp_enqueue_script( 'angular-count-to',
-                     '//rawgit.com/sparkalow/angular-count-to/master/build/angular-count-to.min.js',
-                     array('foundation', 'jquery') );
-
 	// Add the comment-reply library on pages where it is necessary
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
