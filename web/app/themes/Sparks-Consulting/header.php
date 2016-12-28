535e345d5d80a4ca30e6a53241bd9f65bd2219ed
Josh Tipton <josh@blue-bot.com>
2016-12-28 16:47:28 -0500

    Removing Angular Rowing DB Assets

--- web/app/themes/Sparks-Consulting/header.php
+++ web/app/themes/Sparks-Consulting/header.php
@@ -53,8 +53,6 @@
 				get_template_part( 'parts/journal-nav' );
 			elseif (is_page_template('templates/journal-category.php'))
 				get_template_part( 'parts/journal-nav' );
-			elseif (is_page_template('templates/rowing-db.php'))
-				get_template_part( 'parts/rowingdb-nav' );
 			else
 				get_template_part( 'parts/main-nav' );?>
 
